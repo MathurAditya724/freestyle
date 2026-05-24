57eb13eac15a23f0fd207497b59084aa4d3be196
mathuraditya724 <mathuraditya724@gmail.com>
2026-05-24 13:37:09 +0530

    feat: added shadcn

--- apps/electron/src/renderer/src/main.tsx
+++ apps/electron/src/renderer/src/main.tsx
@@ -1,11 +1,19 @@
-import "./main.css";
+import "./globals.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "next-themes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
